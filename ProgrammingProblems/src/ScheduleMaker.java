653cf2a376d914857bf7e382c074ef84cfa8d4c1
Anna AD <anna.arpacidusseau@gmail.com>
2018-09-21 15:19:22 -0500

    base for schedulemaker

---
+++ ProgrammingProblems/src/ScheduleMaker.java
@@ -0,0 +1,12 @@
+
+public class ScheduleMaker {
+
+	public static void main(String[] args) {
+
+	}
+	
+	public static String solution(String startTime, int bufferTime, String[][] activites) {
+		
+	}
+
+}
