f944f3387d6e1821c47a4cd57771fb4a40f2ac9f
Anna AD <anna.arpacidusseau@gmail.com>
2018-09-21 14:57:58 -0500

    update

--- ProgrammingProblems/src/PrintPlanetGreeting.java
+++ ProgrammingProblems/src/PrintPlanetGreeting.java
@@ -1,4 +1,12 @@
 
 public class PrintPlanetGreeting {
-
+	
+	public static void main(String args[]) {
+		String planet = "Earth";
+		System.out.println(solution(planet));
+	}
+	
+	public static String solution(String planet) {
+		return "Hello " + planet + "!";
+	}
 }
