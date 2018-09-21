b5d4cdbed9925df95a7c09ebde180c15ebf1dbe0
Anna AD <anna.arpacidusseau@gmail.com>
2018-09-21 15:09:57 -0500

    finished infection problem

--- ProgrammingProblems/src/MapInfectionSpread.java
+++ ProgrammingProblems/src/MapInfectionSpread.java
@@ -2,8 +2,64 @@
 public class MapInfectionSpread {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int[][] map = {{0,0,0,0,0},
+					   {0,1,1,0,1},
+					   {0,2,1,0,1},
+					   {0,0,0,0,1},
+					   {1,0,0,1,2}};
+		
+		int[][] output = solution(map);
+		
+		for(int[] i : output) {
+			for(int j : i) {
+				System.out.print(j);
+			}
+			System.out.println();
+		}
+	
+		
 
 	}
-
+	
+	public static int[][] solution(int[][] map){
+		for(int i = 0; i < map.length; i++) {
+			for(int j = 0; j < map[0].length; j++) {
+				if(map[i][j] == 2) {
+					spreadInfection(i,j,map);
+				}
+			}
+		}
+		
+		return map;
+	}
+	
+	public static void spreadInfection(int i, int j, int[][] map) {
+		if(i < map.length-1) {
+			if(map[i+1][j] == 1) {
+				map[i+1][j] = 2;
+				spreadInfection(i+1,j,map);
+			}
+		}
+		
+		if(j < map.length-1) {
+			if(map[i][j+1] == 1) {
+				map[i][j+1] = 2;
+				spreadInfection(i,j+1,map);
+			}
+		}
+		
+		if(i > 0) {
+			if(map[i-1][j] == 1) {
+				map[i-1][j] = 2;
+				spreadInfection(i-1,j,map);
+			}
+		}
+		
+		if(j > 0) {
+			if(map[i][j-1] == 1) {
+				map[i][j-1] = 2;
+				spreadInfection(i,j-1,map);
+			}
+		}
+	}
 }
