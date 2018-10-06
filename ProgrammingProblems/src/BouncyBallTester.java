e6ed1624553e811d68b27326e82c8ed55ee3912e
Anna AD <anna.arpacidusseau@gmail.com>
2018-10-06 17:58:11 -0500

    adjusted in and out
    for bouncyballtester

--- ProgrammingProblems/src/BouncyBallTester.java
+++ ProgrammingProblems/src/BouncyBallTester.java
@@ -1,9 +1,15 @@
+import java.util.Scanner;
 
 public class BouncyBallTester {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println(solution(1f,0.5f));
+		Scanner sc = new Scanner(System.in);
+		float i = sc.nextFloat();
+		float j = sc.nextFloat();
+
+		
+		System.out.println(solution(i,j));
 	}
 	
 	public static int solution(float altitude, float nextBounceMultiplier) {
