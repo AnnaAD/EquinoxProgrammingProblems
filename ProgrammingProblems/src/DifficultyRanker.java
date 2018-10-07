985647b3f6bc6da57e63aeb277eab839306bab31
Anna AD <anna.arpacidusseau@gmail.com>
2018-10-07 14:13:17 -0500

    fixed difficulty ranker and added scanner to it

--- ProgrammingProblems/src/DifficultyRanker.java
+++ ProgrammingProblems/src/DifficultyRanker.java
@@ -1,22 +1,31 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class DifficultyRanker {
 	public static void main (String args[]) {
-		String[][] data = {{"catProblem","32"},{"catProblem","80"},{"dogProblem","75"}};
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		sc.nextLine();
+		String[][] data = new String[n][];
+		for(int i = 0; i < n; i++) {
+			data[i] = sc.nextLine().split(" ");
+		}
 		System.out.println(solution(data));
 	}
 	
 	public static String solution(String[][] problemList) {
 		ArrayList<String> problemTypes = new ArrayList<String>();
 		ArrayList<Integer> problemTimes = new ArrayList<Integer>();
-		
+		ArrayList<Integer> problemFrequency = new ArrayList<Integer>();
 		for(int i = 0; i < problemList.length; i++) {
 			if(!problemTypes.contains(problemList[i][0])) {
 				problemTypes.add(problemList[i][0]);
 				problemTimes.add(Integer.parseInt(problemList[i][1]));
+				problemFrequency.add(1);
 			} else {
 				int index = problemTypes.indexOf(problemList[i][0]);
-				problemTimes.set(index,problemTimes.get(index) + Integer.parseInt(problemList[i][1]));
+				problemFrequency.set(index, problemFrequency.get(index) + 1);
+				problemTimes.set(index,(problemTimes.get(index) + Integer.parseInt(problemList[i][1]))/problemFrequency.get(index));
 			}
 		}
 		
